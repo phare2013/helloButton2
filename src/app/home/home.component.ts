2536ec24766487e78dc534a13df1f38b99ece347
Paris Hare <parishare@Pariss-MacBook-Pro.local>
2019-02-14 02:27:20 -0600

    the app is now working on android and ios. I changed audio plugin to nativescript-audio. We may not have needed to do this. We can test it later.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { GestureEventData } from "tns-core-modules/ui/gestures";
 const timerModule = require("tns-core-modules/timer");
-const soundModule = require("nativescript-sound");
+import { TNSPlayer } from 'nativescript-audio';
 
 @Component({
     selector: "Home",
@@ -14,10 +14,27 @@ export class HomeComponent implements OnInit {
     public buttonUpImg:string = "~/app/imgs/red_button_up.png";
     public buttonDownImg:string = "~/app/imgs/red_button_down.png";
     public buttonPressed:boolean = false;
-    public tada = soundModule.create("~/app/snd/test.mp3")
+    private _player: TNSPlayer;
+    // public tada = soundModule.create("~/app/snd/test.mp3")
 
     constructor() {
         // Use the component constructor to inject providers.
+        this._player = new TNSPlayer();
+        this._player.debug = true; // set true to enable TNSPlayer console logs for debugging.
+        this._player
+          .initFromFile({
+            audioFile: "~/app/snd/test.mp3", // ~ = app directory
+            loop: false,
+            completeCallback: this._trackComplete.bind(this),
+            errorCallback: this._trackError.bind(this)
+          })
+          .then(() => {
+            this._player.getAudioTrackDuration().then(duration => {
+              // iOS: duration is in seconds
+              // Android: duration is in milliseconds
+              console.log(`song duration:`, duration);
+            });
+          });
     }
 
     ngOnInit(): void {
@@ -27,9 +44,23 @@ export class HomeComponent implements OnInit {
     buttonClicked(args: GestureEventData): void {
         console.log("button was pressed:" + args.eventName);
         this.buttonImg = this.buttonDownImg;
-        this.tada.play();
+         this._player.play();
         timerModule.setTimeout(() => {
             this.buttonImg = this.buttonUpImg;
         },100 );
     }
+
+    private _trackComplete(args: any) {
+        console.log('reference back to player:', args.player);
+        // iOS only: flag indicating if completed succesfully
+        console.log('whether song play completed successfully:', args.flag);
+      }
+     
+      private _trackError(args: any) {
+        console.log('reference back to player:', args.player);
+        console.log('the error:', args.error);
+        // Android only: extra detail on error
+        console.log('extra info on the error:', args.extra);
+      }
+    
 }
